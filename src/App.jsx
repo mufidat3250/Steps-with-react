26970ef52dafa7500f5636ab21a75bed84154416
mufidat3250 <wahabmufidat1919@gmail.com>
2023-10-12 23:07:04 +0100

    feat: adding close icon

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(0) 
+  const [isOpen, setIsOpen] = useState(true)
 
   const handleNext = () => {
     if(step < 3) {
@@ -26,7 +27,11 @@ function App() {
 
  
   return (
-   <div className='steps'>
+  <div className='app'>
+    <div className='close' onClick={()=> setIsOpen(!isOpen)}>&times;</div>
+
+    <div className='steps-wrapper'>
+    {isOpen &&  <div className='steps'>
     <div className='numbers'>
         <div className={step >= 1 ? 'active':'' }>1</div>
         <div className={step >= 2 ? 'active' : ''}>2</div>
@@ -37,7 +42,9 @@ function App() {
       <button onClick={handlePrev}>Previous</button>
       <button onClick={handleNext}>Next</button>
     </div>
-   </div>
+   </div>}
+    </div>
+  </div>
   )
 }
 
